47e66846e776205134b0f97bb13fdfbccd190137
Emilia <81705094+isemilia@users.noreply.github.com>
2024-05-26 16:38:05 +0500

    Set up form

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <form>
+    <label>
+      Email:
+      <input type="email" required v-model="email">
+    </label>
+    <label>
+      Password:
+      <input type="password" required v-model="password">
+    </label>
+    <p>{{email}}</p>
+    <p>{{password}}</p>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      password: '',
+      email: ''
+    }
+  }
+}
+</script>
+
+<style>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 0 0 25px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+  width: 100%;
+}
+input {
+  margin-top: 5px;
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+</style>
